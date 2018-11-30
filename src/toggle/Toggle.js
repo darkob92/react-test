fb5e6ec95461379c2fea33c180df7d10ff711488
Darko <darko.bogovic92@gmail.com>
2018-11-30 19:31:59 +0100

    adding component

---
+++ src/toggle/Toggle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './toggle.css';
+
+const toggle = () => {
+  return(
+    <button className="f6 link dim ph3 pv2 mb2 dib white bg-dark-blue toggle">Toggle</button>
+  )
+}
+
+export default toggle;
